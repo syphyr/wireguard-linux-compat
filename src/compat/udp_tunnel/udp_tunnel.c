86edb3f6fea2c693b2d92f1623389717d2e32abf
syphyr <syphyr@gmail.com>
2020-12-13 23:12:33 +0100

    net: Fix compile error for sk_no_check
    This has been backported to 3.10
    
    Fixes: net: Split sk_no_check into sk_no_check_{rx,tx}

--- src/compat/udp_tunnel/udp_tunnel.c
+++ src/compat/udp_tunnel/udp_tunnel.c
@@ -61,7 +61,7 @@ int udp_sock_create4(struct net *net, struct udp_port_cfg *cfg,
 			goto error;
 	}
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 16, 0)
+#if 0
 	sock->sk->sk_no_check = !cfg->use_udp_checksums;
 #else
 	sock->sk->sk_no_check_tx = !cfg->use_udp_checksums;
@@ -290,7 +290,7 @@ int udp_sock_create6(struct net *net, struct udp_port_cfg *cfg,
 	if (err < 0)
 		goto error;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 16, 0)
+#if 0
 	sock->sk->sk_no_check = !cfg->use_udp_checksums;
 #else
 	udp_set_no_check6_tx(sock->sk, !cfg->use_udp6_tx_checksums);
